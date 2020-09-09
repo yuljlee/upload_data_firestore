868df89b1980a0c4fb6c52827ddf900ed8faf920
Yul Jae <lee.yuljae@koreadaily.com>
2020-09-09 14:46:31 -0700

    commit on test

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const firestore = admin.firestore();
 const settings = {timestampsInSnapshots: true};
 firestore.settings(settings);
 
+
+
+
 if (data && (typeof data === "object")) {
     Object.keys(data).forEach(docKey => {
         firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
